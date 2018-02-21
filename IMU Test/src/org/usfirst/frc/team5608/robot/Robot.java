e9254b5988910fb10514fb8518c9e91eb0eda889
slay-maker <myslaymaker@gmail.com>
2018-02-21 17:51:55 -0500

    Failed Commit

---
+++ IMU Test/src/org/usfirst/frc/team5608/robot/Robot.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team5608.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+  public static final ADIS16448_IMU imu = new ADIS16448_IMU();
+  
+  @Override
+  public void robotPeriodic() { 
+    SmartDashboard.putNumber("Gyro-X", imu.getAngleX());
+    SmartDashboard.putNumber("Gyro-Y", imu.getAngleY());
+    SmartDashboard.putNumber("Gyro-Z", imu.getAngleZ());
+    
+    SmartDashboard.putNumber("Accel-X", imu.getAccelX());
+    SmartDashboard.putNumber("Accel-Y", imu.getAccelY());
+    SmartDashboard.putNumber("Accel-Z", imu.getAccelZ());
+    
+    SmartDashboard.putNumber("Pitch", imu.getPitch());
+    SmartDashboard.putNumber("Roll", imu.getRoll());
+    SmartDashboard.putNumber("Yaw", imu.getYaw());
+    
+    SmartDashboard.putNumber("Pressure: ", imu.getBarometricPressure());
+    SmartDashboard.putNumber("Temperature: ", imu.getTemperature()); 
+  }
+}
--- Master 2-20-18/src/org/usfirst/frc/team5608/robot/Robot.java
+++ Master 2-20-18/src/org/usfirst/frc/team5608/robot/Robot.java
@@ -18,8 +18,24 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 @SuppressWarnings("deprecation")
 public class Robot extends SampleRobot {
+	private static final String kDefaultJoystick = "Logitech Extreme";
+	private static final String kXboxJoystick = "Xbox";
+	private Joystick m_joystickExtreme = new Joystick(0);
+	private XboxController m_joystickXbox = new XboxController(1);
+	private SendableChooser<String> m_joystickChooser = new SendableChooser<>();
+	private TalonSRX m_leftWheelMotor = new TalonSRX(CANMap.kLeftWheelMotor.getPort());
+	private TalonSRX m_rightWheelMotor = new TalonSRX(CANMap.kRightWheel.getPort());
+	private TalonSRX m_armMotor = new TalonSRX(CANMap.kArms.getPort());
+	private TalonSRX m_cartMotor1 = new TalonSRX(CANMap.kLifting1.getPort());
+	private TalonSRX m_cartMotor2 = new TalonSRX(CANMap.kLifting2.getPort());
+	private TalonSRX m_climbingMotor = new TalonSRX(CANMap.kClimbing.getPort());
+	private TalonSRX m_leftGrabberMotor = new TalonSRX(CANMap.kLeftGrabber.getPort());
+	private TalonSRX m_rightGrabberMotor = new TalonSRX(CANMap.kRightGrabber.getPort());
+	private final double m_deadband = 0.02;
+	private final double m_maxOutput = 1.0;
+	private static final boolean kDriveSquared = false;
 	private enum CANMap {
-		kPDP(0), kLeftWheel(1), kRightWheel(2);
+		kPDP(0), kArms(3), kLeftWheelMotor(5), kRightWheel(7), kLifting1(9), kLifting2(6), kClimbing(4), kLeftGrabber(1), kRightGrabber(2);
 		int m_portNumber;
 		CANMap(int port){
 			this.m_portNumber = port;
@@ -28,16 +44,14 @@ public class Robot extends SampleRobot {
 			return this.m_portNumber;
 		}
 	}
-	private static final String kDefaultJoystick = "Logitech Extreme";
-	private static final String kXboxJoystick = "Xbox";
-	private Joystick m_joystickExtreme = new Joystick(0);
-	private XboxController m_joystickXbox = new XboxController(1);
-	private SendableChooser<String> m_joystickChooser = new SendableChooser<>();
-	private TalonSRX m_leftWheel = new TalonSRX(CANMap.kLeftWheel.getPort());
-	private TalonSRX m_rightWheel = new TalonSRX(CANMap.kRightWheel.getPort());
-	private final double m_deadband = 0.02;
-	private final double m_maxOutput = 1.0;
-	private static final boolean kDriveSquared = false;
+
+	//	private void otherThanDrive() {
+	//		if(liftingTrigger) {
+	//			m_lifting1.set(ControlMode.PercentOutput, kLiftingOutput);
+	//			m_lifting2.set(ControlMode.PercentOutput, kLiftingOutput);
+	//		}
+	//		if()
+	//	}
 
 	public Robot() {}
 
@@ -50,81 +64,86 @@ public class Robot extends SampleRobot {
 
 	@Override
 	public void operatorControl() {
-		String joystickSelected = m_joystickChooser.getSelected();
-		System.out.println("Auto selected: " + joystickSelected);
-		switch (joystickSelected) {
-			case kXboxJoystick:
-				while (isOperatorControl() && isEnabled()) {
-					arcadeDrive(m_joystickXbox.getRawAxis(5), combine(m_joystickXbox.getRawAxis(9), m_joystickXbox.getRawAxis(10)));
-					Timer.delay(0.005);
-				}
-				break;
-			case kDefaultJoystick:
-			default:
-				while (isOperatorControl() && isEnabled()) {
-					arcadeDrive(m_joystickExtreme.getRawAxis(0), m_joystickExtreme.getRawAxis(2));
-					Timer.delay(0.005);
-				}
-				break;
+		String selectedJoystick = m_joystickChooser.getSelected();
+		System.out.println("Auto selected: " + selectedJoystick);
+		switch (selectedJoystick) {
+		case kXboxJoystick:
+			while (isOperatorControl() && isEnabled()) {
+				arcadeDrive(m_joystickXbox.getRawAxis(5), combine(m_joystickXbox.getRawAxis(9), m_joystickXbox.getRawAxis(10)));
+				Timer.delay(0.005);
+			}
+			break;
+		default:
+		case kDefaultJoystick:
+			while (isOperatorControl() && isEnabled()) {
+				arcadeDrive(m_joystickExtreme.getRawAxis(0), m_joystickExtreme.getRawAxis(2));
+				Timer.delay(0.005);
+			}
+			break;
 		}
 	}
-	
+
+	@Override
+	public void test() {
+
+	}
+
 	private double combine(double neg, double pos) {
 		return 0;
 	}
-	
+
 	private double limit(double value) {
-	    if (value > 1.0) {
-	      return 1.0;
-	    }
-	    if (value < -1.0) {
-	      return -1.0;
-	    }
-	    return value;
-	  }
-	
+		if (value > 1.0) {
+			return 1.0;
+		}
+		if (value < -1.0) {
+			return -1.0;
+		}
+		return value;
+	}
+
 	private double applyDeadband(double value, double deadband) {
-	    if (Math.abs(value) > deadband) {
-	      if (value > 0.0) {
-	        return (value - deadband) / (1.0 - deadband);
-	      } else {
-	        return (value + deadband) / (1.0 - deadband);
-	      }
-	    } else {
-	      return 0.0;
-	    }
-	  }
-	
-	public void arcadeDrive(double xSpeed, double zRotation) {
-	    xSpeed = limit(xSpeed);
-	    xSpeed = applyDeadband(xSpeed, m_deadband);
-	    zRotation = limit(zRotation);
-	    zRotation = applyDeadband(zRotation, m_deadband);
-	    if (kDriveSquared) {
-	      xSpeed = Math.copySign(xSpeed * xSpeed, xSpeed);
-	      zRotation = Math.copySign(zRotation * zRotation, zRotation);
-	    }
-	    double leftMotorOutput;
-	    double rightMotorOutput;
-	    double maxInput = Math.copySign(Math.max(Math.abs(xSpeed), Math.abs(zRotation)), xSpeed);
-	    if (xSpeed >= 0.0) {
-	      if (zRotation >= 0.0) {
-	        leftMotorOutput = maxInput;
-	        rightMotorOutput = xSpeed - zRotation;
-	      } else {
-	        leftMotorOutput = xSpeed + zRotation;
-	        rightMotorOutput = maxInput;
-	      }
-	    } else {
-	      if (zRotation >= 0.0) {
-	        leftMotorOutput = xSpeed + zRotation;
-	        rightMotorOutput = maxInput;
-	      } else {
-	        leftMotorOutput = maxInput;
-	        rightMotorOutput = xSpeed - zRotation;
-	      }
-	    }
-	    m_leftWheel.set(ControlMode.PercentOutput, limit(leftMotorOutput) * m_maxOutput);
-	    m_rightWheel.set(ControlMode.PercentOutput, -limit(rightMotorOutput) * m_maxOutput);
-	  }
+		if (Math.abs(value) > deadband) {
+			if (value > 0.0) {
+				return (value - deadband) / (1.0 - deadband);
+			} else {
+				return (value + deadband) / (1.0 - deadband);
+			}
+		} else {
+			return 0.0;
+		}
+	}
+
+	private void arcadeDrive(double xSpeed, double zRotation) {
+		xSpeed = limit(xSpeed);
+		xSpeed = applyDeadband(xSpeed, m_deadband);
+		zRotation = limit(zRotation);
+		zRotation = applyDeadband(zRotation, m_deadband);
+		if (kDriveSquared) {
+			xSpeed = Math.copySign(xSpeed * xSpeed, xSpeed);
+			zRotation = Math.copySign(zRotation * zRotation, zRotation);
+		}
+		double leftMotorOutput;
+		double rightMotorOutput;
+		double maxInput = Math.copySign(Math.max(Math.abs(xSpeed), Math.abs(zRotation)), xSpeed);
+		if (xSpeed >= 0.0) {
+			if (zRotation >= 0.0) {
+				leftMotorOutput = maxInput;
+				rightMotorOutput = xSpeed - zRotation;
+			} else {
+				leftMotorOutput = xSpeed + zRotation;
+				rightMotorOutput = maxInput;
+			}
+		} else {
+			if (zRotation >= 0.0) {
+				leftMotorOutput = xSpeed + zRotation;
+				rightMotorOutput = maxInput;
+			} else {
+				leftMotorOutput = maxInput;
+				rightMotorOutput = xSpeed - zRotation;
+			}
+		}
+		m_leftWheelMotor.set(ControlMode.PercentOutput, limit(leftMotorOutput) * m_maxOutput);
+		m_rightWheelMotor.set(ControlMode.PercentOutput, -limit(rightMotorOutput) * m_maxOutput);
+	}
 }
